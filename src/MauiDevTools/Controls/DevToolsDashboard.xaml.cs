efaffe7cb27fe33ec0e86640a66e4cca2a595bfe
Javier Suárez <javiersuarezruiz@hotmail.com>
2024-05-12 11:31:28 +0200

    More changes

--- src/MauiDevTools/Controls/DevToolsDashboard.xaml.cs
+++ src/MauiDevTools/Controls/DevToolsDashboard.xaml.cs
@@ -1,5 +1,4 @@
 using CommunityToolkit.Maui.Views;
-using Microsoft.Maui.Platform;
 
 namespace MauiDevTools.Controls;
 
@@ -34,25 +33,5 @@ public partial class DevToolsDashboard : Popup
         }
 
         PerfOverlay?.Invoke(this, EventArgs.Empty);
-    }
-
-    void DebugPaintTapped(object sender, TappedEventArgs e)
-    {
-        var random = new Random();
-
-        Microsoft.Maui.Handlers.ElementHandler.ElementMapper.AppendToMapping("DebugPaint", (handler, view) =>
-        {
-#if ANDROID
-             
-            if (handler.PlatformView is Android.Views.View aView)
-            {     
-                aView.Background = null;
-                var color = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));     
-                aView.SetBackgroundColor(color.ToPlatform());
-            }
-#endif
-        });
-
-        DebugPaint?.Invoke(this, EventArgs.Empty);
     }
 }
